c9521a5df3d9e967aa04124d3cb305f4b444361a
Binnukrishna <binnukrishna5233@gmail.com>
2024-06-06 10:45:06 +0530

    content : updated

--- src/javaScript/index.js
+++ src/javaScript/index.js
@@ -1,6 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   const st_icon = document.querySelector(".container .iphone_border .st_icon");
   const ht_icon = document.querySelector(".container .iphone_border .ht_icon");
+  const multi_icon = document.querySelector(
+    ".container .iphone_border .multi_icon"
+  );
+  const bga_icon = document.querySelector(
+    ".container .iphone_border .bga_icon"
+  );
+  const cro_icon = document.querySelector(
+    ".container .iphone_border .cro_icon"
+  );
+  const dc_icon = document.querySelector(".container .iphone_border .dc_icon");
+  const sold_icon = document.querySelector(
+    ".container .iphone_border .sold_icon"
+  );
+  const rs_icon = document.querySelector(".container .iphone_border .rs_icon");
   const nav_pill = document.querySelector(
     ".container .iphone_mockup .nav_pill"
   );
@@ -10,6 +24,16 @@ document.addEventListener("DOMContentLoaded", () => {
   const ht_page = document.querySelector(
     ".container .iphone_border .hardware_training"
   );
+  const multimeter_page = document.querySelector(
+    ".container .iphone_border .multimeter"
+  );
+  const bga_page = document.querySelector(".container .iphone_border .bga");
+  const cro_page = document.querySelector(".container .iphone_border .cro");
+  const dc_page = document.querySelector(".container .iphone_border .dc");
+  const sold_page = document.querySelector(
+    ".container .iphone_border .soldering"
+  );
+  const rs_page = document.querySelector(".container .iphone_border .rework");
   const app_open = document.querySelector(
     ".container .iphone_border .open_app_section"
   );
@@ -26,10 +50,52 @@ document.addEventListener("DOMContentLoaded", () => {
     nav_pill.classList.add("open");
   });
 
+  multi_icon.addEventListener("click", () => {
+    app_open.classList.add("open");
+    multimeter_page.classList.add("open");
+    nav_pill.classList.add("open");
+  });
+
+  bga_icon.addEventListener("click", () => {
+    app_open.classList.add("open");
+    bga_page.classList.add("open");
+    nav_pill.classList.add("open");
+  });
+
+  cro_icon.addEventListener("click", () => {
+    app_open.classList.add("open");
+    cro_page.classList.add("open");
+    nav_pill.classList.add("open");
+  });
+
+  dc_icon.addEventListener("click", () => {
+    app_open.classList.add("open");
+    dc_page.classList.add("open");
+    nav_pill.classList.add("open");
+  });
+
+  sold_icon.addEventListener("click", () => {
+    app_open.classList.add("open");
+    sold_page.classList.add("open");
+    nav_pill.classList.add("open");
+  });
+
+  rs_icon.addEventListener("click", () => {
+    app_open.classList.add("open");
+    rs_page.classList.add("open");
+    nav_pill.classList.add("open");
+  });
+
   nav_pill.addEventListener("click", () => {
     app_open.classList.remove("open");
     nav_pill.classList.remove("open");
+    multimeter_page.classList.remove("open");
     st_page.classList.remove("open");
     ht_page.classList.remove("open");
+    bga_page.classList.remove("open");
+    cro_page.classList.remove("open");
+    dc_page.classList.remove("open");
+    sold_page.classList.remove("open");
+    rs_page.classList.remove("open");
   });
 });
